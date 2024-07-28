d21cd38dd0f93eaf55d1ec0a2d8c6229a8fcf237
Zhiyong Fang <zhiyong.fang.1997@gmail.com>
2024-07-28 15:17:40 -0500

    minor hash optim

--- src/hash/hashes.rs
+++ src/hash/hashes.rs
@@ -1,7 +1,4 @@
-use sha2::{
-    digest::Output,
-    Digest, Sha256,
-};
+use sha2::{digest::Output, Digest, Sha256};
 #[derive(Debug, Clone, Default)]
 pub struct SHA256hasher {
     pub h: Sha256,
@@ -24,7 +21,7 @@ impl SHA256hasher {
     // }
 
     pub fn hash(&mut self, output: &mut [u8], input: &[u8]) {
-        self.h.update(&input);
+        self.h.update(input);
         self.h
             .finalize_into_reset(Output::<Sha256>::from_mut_slice(output));
     }
@@ -35,7 +32,7 @@ impl SHA256hasher {
     // }
 
     pub fn hash_inplace(&mut self, buffer: &mut [u8]) {
-        self.h.update(&buffer);
+        self.h.update(&*buffer);
         self.h
             .finalize_into_reset(Output::<Sha256>::from_mut_slice(buffer));
     }
