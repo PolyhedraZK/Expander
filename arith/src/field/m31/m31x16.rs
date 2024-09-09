9f17d6eeeb9f38871aecf09cc91f133489af0709
Tiancheng Xie <tc@polyhedra.network>
2024-09-09 11:11:46 -0700

    Update m31x16.rs
    Signed-off-by: Tiancheng Xie <tc@polyhedra.network>

--- arith/src/field/m31/m31x16.rs
+++ arith/src/field/m31/m31x16.rs
@@ -12,34 +12,3 @@ cfg_if::cfg_if! {
 
 #[cfg(target_arch = "aarch64")]
 pub type M31x16 = super::m31_neon::NeonM31;
-
-/*
-use raw_cpuid::CpuId;
-
-fn has_avx512() {
-    let cpuid = CpuId::new();
-
-    if let Some(feature_info) = cpuid.get_extended_feature_info() {
-        if feature_info.has_avx512f() {
-            return true;
-        }
-    } else {
-        return false;
-    }
-}
-*/
-
-/*
-#[cfg(target_arch = "x86_64")]
-{
-    #[cfg(feature = "avx256")]
-    pub type M31x16 = super::m31_avx256::AVXM31;
-
-    #[cfg(not(feature = "avx256"))]
-    if has_avx512() {
-        pub type M31x16 = super::m31_avx::AVXM31;
-    } else {
-        pub type M31x16 = super::m31_avx256::AVXM31;
-    }
-}
-*/
