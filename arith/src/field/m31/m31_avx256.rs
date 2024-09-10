c50bdb462aa5ac0c1243e2a00a443cc4bfe8a74e
Tiancheng Xie <tc@polyhedra.network>
2024-09-10 01:55:53 -0700

    fmt

--- arith/src/field/m31/m31_avx256.rs
+++ arith/src/field/m31/m31_avx256.rs
@@ -282,13 +282,13 @@ impl SimdField for AVXM31 {
     fn pack_size() -> usize {
         M31_PACK_SIZE
     }
-    
+
     fn pack(base_vec: &[Self::Scalar]) -> Self {
         debug_assert_eq!(base_vec.len(), M31_PACK_SIZE);
         let ret: [Self::Scalar; M31_PACK_SIZE] = base_vec.try_into().unwrap();
         unsafe { transmute(ret) }
     }
-    
+
     fn unpack(&self) -> Vec<Self::Scalar> {
         let ret = unsafe { transmute::<[__m256i; 2], [Self::Scalar; M31_PACK_SIZE]>(self.v) };
         ret.to_vec()
