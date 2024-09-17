d8f990910e7f21ded442f9e771fdab52ac1cab61
Zhiyong Fang <zhiyong.fang.1997@gmail.com>
2024-09-16 21:51:34 -0500

    try mpi install

--- scripts/install.py
+++ scripts/install.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     else:
         subprocess.run(["wget", "https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-5.0.5.tar.gz"])
         subprocess.run(["tar", "xf", "openmpi-5.0.5.tar.bz2"])
-        subprocess.run(["cd", "openmpi-5.0.5"])
+        subprocess.run(["cd", "openmpi-5.0.5"], shell=True)
         subprocess.run(["./configure", "--prefix=/tmp"])
         subprocess.run(["make", "-j", "all"])
         subprocess.run(["make", "install"])
+        subprocess.run(["cd", ".."], shell=True)
