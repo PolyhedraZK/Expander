72711212f8658edff8c82eb3df000f61294dbecd
zhenfei <zhenfei.zhang@hotmail.com>
2024-09-19 10:00:48 -0400

    update

--- arith/mersenne31/src/tests.rs
+++ arith/mersenne31/src/tests.rs
@@ -10,6 +10,35 @@ use crate::M31Ext3;
 use crate::M31Ext3x16;
 use crate::{M31x16, M31};
 
+fn get_avx_version() -> &'static str {
+    if cfg!(all(target_arch = "x86_64", target_feature = "avx512f")) {
+        return "AVX512";
+    } else if cfg!(all(
+        target_arch = "x86_64",
+        not(target_feature = "avx512f"),
+        target_feature = "avx2"
+    )) {
+        return "AVX2 (256-bit)";
+    } else if cfg!(target_arch = "aarch64") {
+        return "arm64";
+    }
+    "Unknown"
+}
+
+#[test]
+fn test_avx_version() {
+    let avx_version = get_avx_version();
+    println!("Current AVX version: {}", avx_version);
+    assert!([
+        "AVX512",
+        "AVX2 (256-bit)",
+        "AVX (256-bit)",
+        "No AVX (Fallback)",
+        "Not x86_64 architecture"
+    ]
+    .contains(&avx_version));
+}
+
 #[test]
 fn test_base_field() {
     random_field_tests::<M31>("M31".to_string());
