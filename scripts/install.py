71976ec1bce65cdd45258717720f83371b4f6932
zhenfei <zhenfei.zhang@hotmail.com>
2024-10-10 15:20:55 -0400

    ping specific version of openmpi

--- scripts/install.py
+++ scripts/install.py
@@ -3,6 +3,6 @@ from sys import platform
 
 if __name__ == "__main__":
     if platform == "darwin": # mac os
-        subprocess.run(["brew", "install", "openmpi"])
+        subprocess.run(["brew", "install", "openmpi@5.0.3"])
     else:
         pass # Do nothing, assuming mpi has already been installed
