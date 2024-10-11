c12db8dd2e363b77ac10b25c00a79e6011e8d83b
zhenfei <zhenfei.zhang@hotmail.com>
2024-10-11 11:09:20 -0400

    wip

--- sumcheck/src/prover_helper.rs
+++ sumcheck/src/prover_helper.rs
@@ -4,5 +4,6 @@ mod simd_gate;
 mod sumcheck_gkr_square;
 mod sumcheck_gkr_vanilla;
 
+pub(crate) use product_gate::SumcheckProductGateHelper;
 pub(crate) use sumcheck_gkr_square::SumcheckGkrSquareHelper;
 pub(crate) use sumcheck_gkr_vanilla::SumcheckGkrVanillaHelper;
