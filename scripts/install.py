3655e4d1ae47a7b9bae52d06bb158bdbf341ae54
zhenfei <zhenfei.zhang@hotmail.com>
2025-01-13 22:09:39 -0500

    the old fashion

--- scripts/install.py
+++ scripts/install.py
@@ -5,5 +5,4 @@ if __name__ == "__main__":
     if platform == "darwin": # mac os
         subprocess.run(["brew", "install", "gcc", "make", "openmpi"])
     else:
-        subprocess.run(["sudo", "apt-get", "update"])
         subprocess.run(["sudo", "apt-get", "install", "-y", "build-essential"])
