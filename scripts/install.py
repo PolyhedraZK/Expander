31fb26f20eaa5e4f026b97653fd2d5505dfdcb78
zhenfei <zhenfei.zhang@hotmail.com>
2025-01-13 19:23:42 -0500

    try...

--- scripts/install.py
+++ scripts/install.py
@@ -3,6 +3,7 @@ from sys import platform
 
 if __name__ == "__main__":
     if platform == "darwin": # mac os
-        subprocess.run(["brew", "install", "openmpi"])
+        subprocess.run(["brew", "install", "gcc", "make", "openmpi"])
     else:
-        pass # Do nothing, assuming mpi has already been installed
+        subprocess.run(["sudo", "apt-get", "update"])
+        subprocess.run(["sudo", "apt-get", "install", "-y", "build-essential"])
