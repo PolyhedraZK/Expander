c74080ffa3fdf7fd90248e8a8c149d5c572c2f1f
Hang Su <tonyfloater@gmail.com>
2025-01-20 22:00:38 -0500

    add comments in pack from simd

--- arith/src/simd_field.rs
+++ arith/src/simd_field.rs
@@ -21,6 +21,11 @@ pub trait SimdField: From<Self::Scalar> + Field {
     {
         assert_eq!(simd_vec.len() * PackF::PACK_SIZE, Self::PACK_SIZE);
         let mut temp: Vec<_> = simd_vec.to_vec();
+        // NOTE(HS) this method `pack_from_simd` was introduced with
+        // a motivation of packing multiple GF2x8 into a GF2x64 or a GF2x128.
+        // The reverse here is to ensure that packing all these GF2x8s into a
+        // final GF2x64 can unpack into a vec of GF2s, that is the same order
+        // as a concatenation of unpacked GF2s from GF2x8.
         temp.reverse();
 
         unsafe { *(temp.as_ptr() as *const Self) }
