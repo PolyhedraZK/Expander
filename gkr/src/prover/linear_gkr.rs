3b412b325a23b347108585e4c29fbc78b6c94486
Zhiyong Fang <zhiyong.fang.1997@gmail.com>
2025-02-05 22:16:54 -0600

    add some basic timing

--- gkr/src/prover/linear_gkr.rs
+++ gkr/src/prover/linear_gkr.rs
@@ -1,7 +1,6 @@
 //! This module implements the whole GKR prover, including the IOP and PCS.
 
 use arith::FieldSerde;
-use ark_std::{end_timer, start_timer};
 use circuit::Circuit;
 use config::{Config, GKRConfig, GKRScheme};
 use gkr_field_config::GKRFieldConfig;
@@ -9,6 +8,7 @@ use poly_commit::{ExpanderGKRChallenge, PCSForExpanderGKR, StructuredReferenceSt
 use polynomials::{MultilinearExtension, RefMultiLinearPoly};
 use sumcheck::ProverScratchPad;
 use transcript::{transcript_root_broadcast, Proof, Transcript};
+use utils::timer::Timer;
 
 use crate::{gkr_prove, gkr_square_prove};
 
@@ -16,7 +16,7 @@ use crate::{gkr_prove, gkr_square_prove};
 pub(crate) fn grind<Cfg: GKRConfig>(transcript: &mut Cfg::Transcript, config: &Config<Cfg>) {
     use arith::{Field, FieldSerde};
 
-    let timer = start_timer!(|| format!("grind {} bits", config.grinding_bits));
+    let timer = Timer::new("grinding", config.mpi_config.is_root());
 
     let mut hash_bytes = vec![];
 
@@ -39,7 +39,7 @@ pub(crate) fn grind<Cfg: GKRConfig>(transcript: &mut Cfg::Transcript, config: &C
     }
     transcript.append_u8_slice(&hash_bytes[..32]);
     transcript.unlock_proof();
-    end_timer!(timer);
+    timer.stop();
 }
 
 #[derive(Default)]
@@ -83,9 +83,10 @@ impl<Cfg: GKRConfig> Prover<Cfg> {
         pcs_proving_key: &<<Cfg::PCS as PCSForExpanderGKR<Cfg::FieldConfig, Cfg::Transcript>>::SRS as StructuredReferenceString>::PKey,
         pcs_scratch: &mut <Cfg::PCS as PCSForExpanderGKR<Cfg::FieldConfig, Cfg::Transcript>>::ScratchPad,
     ) -> (<Cfg::FieldConfig as GKRFieldConfig>::ChallengeField, Proof) {
-        let timer = start_timer!(|| "prove");
+        let timer = Timer::new("prover", self.config.mpi_config.is_root());
         let mut transcript = Cfg::Transcript::new();
 
+        let pcs_commit_timer = Timer::new("pcs commitment", self.config.mpi_config.is_root());
         // PC commit
         let commitment = {
             let mle_ref = RefMultiLinearPoly::from_ref(&c.layers[0].input_vals);
@@ -100,6 +101,7 @@ impl<Cfg: GKRConfig> Prover<Cfg> {
         let mut buffer = vec![];
         commitment.serialize_into(&mut buffer).unwrap(); // TODO: error propagation
         transcript.append_commitment(&buffer);
+        pcs_commit_timer.stop();
 
         transcript_root_broadcast(&mut transcript, &self.config.mpi_config);
 
@@ -115,15 +117,18 @@ impl<Cfg: GKRConfig> Prover<Cfg> {
         let mut rsimd = vec![];
         let mut rmpi = vec![];
 
+        let gkr_prove_timer = Timer::new("gkr prove", self.config.mpi_config.is_root());
         if self.config.gkr_scheme == GKRScheme::GkrSquare {
             (_, rx) = gkr_square_prove(c, &mut self.sp, &mut transcript);
         } else {
             (claimed_v, rx, ry, rsimd, rmpi) =
                 gkr_prove(c, &mut self.sp, &mut transcript, &self.config.mpi_config);
         }
+        gkr_prove_timer.stop();
 
         transcript_root_broadcast(&mut transcript, &self.config.mpi_config);
 
+        let pcs_open_timer = Timer::new("pcs open", self.config.mpi_config.is_root());
         // open
         let mle_ref = RefMultiLinearPoly::from_ref(&c.layers[0].input_vals);
         self.prove_input_layer_claim(
@@ -155,7 +160,8 @@ impl<Cfg: GKRConfig> Prover<Cfg> {
             );
         }
 
-        end_timer!(timer);
+        pcs_open_timer.stop();
+        timer.stop();
 
         (claimed_v, transcript.finalize_and_get_proof())
     }
