768f674a7e9fa01e71fc1b481779e03dd913ecf7
Hang Su <tonyfloater@gmail.com>
2025-03-13 18:43:38 -0400

    minor, rust fmt skipping

--- arith/src/fft_field.rs
+++ arith/src/fft_field.rs
@@ -146,7 +146,7 @@ mod fft_test {
 }
 
 // NOTE(HS) acknowledge to https://github.com/EugeneGonzalez/bit_reverse
-#[cfg_attr(rustfmt, rustfmt_skip)]
+#[rustfmt::skip]
 const U8_REVERSE_LOOKUP: [u8; 256] = [
     0,  128, 64, 192, 32, 160,  96, 224, 16, 144, 80, 208, 48, 176, 112, 240,
     8,  136, 72, 200, 40, 168, 104, 232, 24, 152, 88, 216, 56, 184, 120, 248,
