ea70049b90cb522c699e29d82aa0352fe1f22e75
zhenfei <zhenfei.zhang@hotmail.com>
2025-03-15 13:47:37 -0400

    revert raw PCS len (de)ser

--- poly_commit/src/raw.rs
+++ poly_commit/src/raw.rs
@@ -3,6 +3,7 @@ use crate::{
     ExpanderGKRChallenge, PCSForExpanderGKR, PolynomialCommitmentScheme, StructuredReferenceString,
 };
 use arith::{ExtensionField, Field};
+use ethnum::U256;
 use gkr_field_config::GKRFieldConfig;
 use mpi_config::MPIConfig;
 use polynomials::{MultiLinearPoly, MultiLinearPolyExpander, MultilinearExtension};
@@ -19,8 +20,8 @@ impl<F: Field> ExpSerde for RawCommitment<F> {
     const SERIALIZED_SIZE: usize = unimplemented!();
 
     fn serialize_into<W: std::io::Write>(&self, mut writer: W) -> SerdeResult<()> {
-        let len = self.evals.len();
-        writer.write_all(len.to_le_bytes().as_ref())?;
+        let u256_embedded = U256::from(self.evals.len() as u64);
+        u256_embedded.serialize_into(&mut writer)?;
 
         self.evals
             .iter()
@@ -32,9 +33,9 @@ impl<F: Field> ExpSerde for RawCommitment<F> {
     fn deserialize_from<R: std::io::Read>(mut reader: R) -> SerdeResult<Self> {
         let mut v = Self::default();
 
-        let len = usize::deserialize_from(&mut reader)?;
+        let len = U256::deserialize_from(&mut reader)?;
 
-        for _ in 0..len {
+        for _ in 0..len.as_usize() {
             v.evals.push(F::deserialize_from(&mut reader)?);
         }
         Ok(v)
