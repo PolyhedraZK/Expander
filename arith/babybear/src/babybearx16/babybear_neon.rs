b81648ea9755fc2809bd207318656635699376d5
zhenfei <zhenfei.zhang@hotmail.com>
2025-04-01 11:48:25 -0400

    fix clippy

--- arith/babybear/src/babybearx16/babybear_neon.rs
+++ arith/babybear/src/babybearx16/babybear_neon.rs
@@ -48,7 +48,7 @@ impl NeonBabyBear {
             v: unsafe {
                 // Safety: memory representation of [x; BABY_BEAR_PACK_SIZE]
                 // is 16 u32s, which can be reinterpreted as 4 uint32x4_t.
-                transmute([x; BABY_BEAR_PACK_SIZE])
+                transmute::<[BabyBear; BABY_BEAR_PACK_SIZE], [uint32x4_t; 4]>([x; BABY_BEAR_PACK_SIZE])
             },
         }
     }
@@ -93,15 +93,27 @@ impl Field for NeonBabyBear {
     const FIELD_SIZE: usize = 32;
 
     const ZERO: Self = Self {
-        v: unsafe { transmute([BabyBear::ZERO; BABY_BEAR_PACK_SIZE]) },
+        v: unsafe {
+            transmute::<[BabyBear; BABY_BEAR_PACK_SIZE], [uint32x4_t; 4]>(
+                [BabyBear::ZERO; BABY_BEAR_PACK_SIZE],
+            )
+        },
     };
 
     const ONE: Self = Self {
-        v: unsafe { transmute([BabyBear::ONE; BABY_BEAR_PACK_SIZE]) },
+        v: unsafe {
+            transmute::<[BabyBear; BABY_BEAR_PACK_SIZE], [uint32x4_t; 4]>(
+                [BabyBear::ONE; BABY_BEAR_PACK_SIZE],
+            )
+        },
     };
 
     const INV_2: Self = Self {
-        v: unsafe { transmute([BabyBear::INV_2; BABY_BEAR_PACK_SIZE]) },
+        v: unsafe {
+            transmute::<[BabyBear; BABY_BEAR_PACK_SIZE], [uint32x4_t; 4]>(
+                [BabyBear::INV_2; BABY_BEAR_PACK_SIZE],
+            )
+        },
     };
 
     const MODULUS: U256 = BabyBear::MODULUS;
@@ -119,24 +131,14 @@ impl Field for NeonBabyBear {
     }
 
     fn random_unsafe(mut rng: impl RngCore) -> Self {
-        let mut sample = [BabyBear::ZERO; BABY_BEAR_PACK_SIZE];
-        for i in 0..BABY_BEAR_PACK_SIZE {
-            sample[i] = BabyBear::random_unsafe(&mut rng);
+        // Caution: this may not produce uniformly random elements
+        unsafe {
+            let mut data = [0u8; 64];
+            rng.fill_bytes(&mut data);
+            let mut v = transmute::<[u8; 64], [uint32x4_t; 4]>(data);
+            v = mod_reduce_epi32x4_twice(&v);
+            Self { v }
         }
-        let r = Self::pack(&sample);
-        if !r.is_canonical() {
-            panic!("random_unsafe: output is not canonical\n{:?}", r);
-        }
-
-        r
-        // // Caution: this may not produce uniformly random elements
-        // unsafe {
-        //     let mut data = [0u8; 64];
-        //     rng.fill_bytes(&mut data);
-        //     let mut v = transmute::<[u8; 64], [uint32x4_t; 4]>(data);
-        //     v = mod_reduce_epi32x4_twice(&v);
-        //     Self { v }
-        // }
     }
 
     fn random_bool(mut rng: impl RngCore) -> Self {
@@ -183,7 +185,7 @@ impl SimdField for NeonBabyBear {
         let ret: [Self::Scalar; BABY_BEAR_PACK_SIZE] = base_vec.try_into().unwrap();
         Self {
             // Transmute is reinterpreting an array of scalars in Montgomery form to an AVX register
-            v: unsafe { transmute(ret) },
+            v: unsafe { transmute::<[Self::Scalar; BABY_BEAR_PACK_SIZE], [uint32x4_t; 4]>(ret) },
         }
     }
 
@@ -243,6 +245,7 @@ impl Mul<&BabyBear> for NeonBabyBear {
 impl Mul<BabyBear> for NeonBabyBear {
     type Output = NeonBabyBear;
     #[inline(always)]
+    #[allow(clippy::op_ref)]
     fn mul(self, rhs: BabyBear) -> Self::Output {
         self * &rhs
     }
@@ -367,53 +370,162 @@ mod p3_instructions {
         y
     }
 
+    /// Add two vectors of Monty31 field elements in canonical form.
+    /// If the inputs are not in canonical form, the result is undefined.
     #[inline]
     #[must_use]
     pub(super) fn add(lhs: uint32x4_t, rhs: uint32x4_t) -> uint32x4_t {
+        // We want this to compile to:
+        //      add   t.4s, lhs.4s, rhs.4s
+        //      sub   u.4s, t.4s, P.4s
+        //      umin  res.4s, t.4s, u.4s
+        // throughput: .75 cyc/vec (5.33 els/cyc)
+        // latency: 6 cyc
+
+        //   Let `t := lhs + rhs`. We want to return `t mod P`. Recall that `lhs` and `rhs` are in
+        // `0, ..., P - 1`, so `t` is in `0, ..., 2 P - 2 (< 2^32)`. It suffices to return `t` if
+        // `t < P` and `t - P` otherwise.
+        //   Let `u := (t - P) mod 2^32` and `r := unsigned_min(t, u)`.
+        //   If `t` is in `0, ..., P - 1`, then `u` is in `(P - 1 <) 2^32 - P, ..., 2^32 - 1` and
+        // `r = t`. Otherwise `t` is in `P, ..., 2 P - 2`, `u` is in `0, ..., P - 2 (< P)` and `r =
+        // u`. Hence, `r` is `t` if `t < P` and `t - P` otherwise, as desired.
+
         unsafe {
+            // Safety: If this code got compiled then NEON intrinsics are available.
             let t = vaddq_u32(lhs, rhs);
             let u = vsubq_u32(t, PACKED_MOD);
             vminq_u32(t, u)
         }
     }
 
+    /// Subtract vectors of Monty31 field elements in canonical form.
+    /// If the inputs are not in canonical form, the result is undefined.
     #[inline]
     #[must_use]
     pub(super) fn sub(lhs: uint32x4_t, rhs: uint32x4_t) -> uint32x4_t {
+        // We want this to compile to:
+        //      sub   res.4s, lhs.4s, rhs.4s
+        //      cmhi  underflow.4s, rhs.4s, lhs.4s
+        //      mls   res.4s, underflow.4s, P.4s
+        // throughput: .75 cyc/vec (5.33 els/cyc)
+        // latency: 5 cyc
+
+        //   Let `d := lhs - rhs`. We want to return `d mod P`.
+        //   Since `lhs` and `rhs` are both in `0, ..., P - 1`, `d` is in `-P + 1, ..., P - 1`. It
+        // suffices to return `d + P` if `d < 0` and `d` otherwise.
+        //   Equivalently, we return `d + P` if `rhs > lhs` and `d` otherwise.  Observe that this
+        // permits us to perform all calculations `mod 2^32`, so define `diff := d mod 2^32`.
+        //   Let `underflow` be `-1 mod 2^32` if `rhs > lhs` and `0` otherwise.
+        //   Finally, let `r := (diff - underflow * P) mod 2^32` and observe that
+        // `r = (diff + P) mod 2^32` if `rhs > lhs` and `diff` otherwise, as desired.
         unsafe {
+            // Safety: If this code got compiled then NEON intrinsics are available.
             let diff = vsubq_u32(lhs, rhs);
             let underflow = vcltq_u32(lhs, rhs);
-            vmlsq_u32(diff, underflow, PACKED_MOD)
+            // We really want to emit a `mls` instruction here. The compiler knows that `underflow`
+            // is either 0 or -1 and will try to do an `and` and `add` instead, which is
+            // slower on the M1. The `confuse_compiler` prevents this "optimization".
+            vmlsq_u32(diff, confuse_compiler(underflow), PACKED_MOD)
         }
     }
 
+    /// Negate a vector of Monty31 field elements in canonical form.
+    /// If the inputs are not in canonical form, the result is undefined.
     #[inline]
     #[must_use]
     pub(super) fn neg(val: uint32x4_t) -> uint32x4_t {
+        // We want this to compile to:
+        //      sub   t.4s, P.4s, val.4s
+        //      cmeq  is_zero.4s, val.4s, #0
+        //      bic   res.4s, t.4s, is_zero.4s
+        // throughput: .75 cyc/vec (5.33 els/cyc)
+        // latency: 4 cyc
+
+        // This has the same throughput as `sub(0, val)` but slightly lower latency.
+
+        //   We want to return (-val) mod P. This is equivalent to returning `0` if `val = 0` and
+        // `P - val` otherwise, since `val` is in `0, ..., P - 1`.
+        //   Let `t := P - val` and let `is_zero := (-1) mod 2^32` if `val = 0` and `0` otherwise.
+        //   We return `r := t & ~is_zero`, which is `t` if `val > 0` and `0` otherwise, as desired.
         unsafe {
+            // Safety: If this code got compiled then NEON intrinsics are available.
             let t = vsubq_u32(PACKED_MOD, val);
             let is_zero = vceqzq_u32(val);
             vbicq_u32(t, is_zero)
         }
     }
 
+    // MONTGOMERY MULTIPLICATION
+    //   This implementation is based on [1] but with changes. The reduction is as follows:
+    //
+    // Constants: P < 2^31
+    //            B = 2^32
+    //            μ = P^-1 mod B
+    // Input: -P^2 <= C <= P^2
+    // Output: -P < D < P such that D = C B^-1 (mod P)
+    // Define:
+    //   smod_B(a) = r, where -B/2 <= r <= B/2 - 1 and r = a (mod B).
+    // Algorithm:
+    //   1. Q := smod_B(μ C)
+    //   2. D := (C - Q P) / B
+    //
+    // We first show that the division in step 2. is exact. It suffices to show that C = Q P (mod
+    // B). By definition of Q, smod_B, and μ, we have Q P = smod_B(μ C) P = μ C P = P^-1 C P = C
+    // (mod B).
+    //
+    // We also have C - Q P = C (mod P), so thus D = C B^-1 (mod P).
+    //
+    // It remains to show that D is in the correct range. It suffices to show that -P B < C - Q P <
+    // P B. We know that -P^2 <= C <= P^2 and (-B / 2) P <= Q P <= (B/2 - 1) P. Then
+    // (1 - B/2) P - P^2 <= C - Q P <= (B/2) P + P^2. Now, P < B/2, so B/2 + P < B and
+    // (B/2) P + P^2 < P B; also B/2 - 1 + P < B, so -P B < (1 - B/2) P - P^2.
+    // Hence, -P B < C - Q P < P B as desired.
+    //
+    // [1] Modern Computer Arithmetic, Richard Brent and Paul Zimmermann, Cambridge University
+    // Press,     2010, algorithm 2.7.
+
     #[inline]
     #[must_use]
     fn mulby_mu(val: int32x4_t) -> int32x4_t {
+        // We want this to compile to:
+        //      mul      res.4s, val.4s, MU.4s
+        // throughput: .25 cyc/vec (16 els/cyc)
+        // latency: 3 cyc
+
         unsafe { vmulq_s32(val, PACKED_MU) }
     }
 
     #[inline]
     #[must_use]
     fn get_c_hi(lhs: int32x4_t, rhs: int32x4_t) -> int32x4_t {
-        unsafe { vqdmulhq_s32(lhs, rhs) }
+        // We want this to compile to:
+        //      sqdmulh  c_hi.4s, lhs.4s, rhs.4s
+        // throughput: .25 cyc/vec (16 els/cyc)
+        // latency: 3 cyc
+
+        unsafe {
+            // Get bits 31, ..., 62 of C. Note that `sqdmulh` saturates when the product doesn't fit
+            // in an `i63`, but this cannot happen here due to our bounds on `lhs` and
+            // `rhs`.
+            vqdmulhq_s32(lhs, rhs)
+        }
     }
 
     #[inline]
     #[must_use]
     fn get_qp_hi(lhs: int32x4_t, mu_rhs: int32x4_t) -> int32x4_t {
+        // We want this to compile to:
+        //      mul      q.4s, lhs.4s, mu_rhs.4s
+        //      sqdmulh  qp_hi.4s, q.4s, P.4s
+        // throughput: .5 cyc/vec (8 els/cyc)
+        // latency: 6 cyc
+
         unsafe {
+            // Form `Q`.
             let q = vmulq_s32(lhs, mu_rhs);
+
+            // Gets bits 31, ..., 62 of Q P. Again, saturation is not an issue because `P` is not
+            // -2**31.
             vqdmulhq_s32(q, vreinterpretq_s32_u32(PACKED_MOD))
         }
     }
@@ -435,7 +547,6 @@ mod p3_instructions {
         }
     }
 
-
     #[inline]
     #[must_use]
     fn get_reduced_d(c_hi: int32x4_t, qp_hi: int32x4_t) -> uint32x4_t {
@@ -445,13 +556,13 @@ mod p3_instructions {
         //      mls      res.4s, underflow.4s, P.4s
         // throughput: .75 cyc/vec (5.33 els/cyc)
         // latency: 5 cyc
-    
+
         unsafe {
             let d = vreinterpretq_u32_s32(get_d(c_hi, qp_hi));
-    
-            // Finally, we reduce D to canonical form. D is negative iff `c_hi > qp_hi`, so if that's the
-            // case then we add P. Note that if `c_hi > qp_hi` then `underflow` is -1, so we must
-            // _subtract_ `underflow` * P.
+
+            // Finally, we reduce D to canonical form. D is negative iff `c_hi > qp_hi`, so if
+            // that's the case then we add P. Note that if `c_hi > qp_hi` then
+            // `underflow` is -1, so we must _subtract_ `underflow` * P.
             let underflow = vcltq_s32(c_hi, qp_hi);
             vmlsq_u32(d, confuse_compiler(underflow), PACKED_MOD)
         }
