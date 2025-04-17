41254b73812b602dd6bdf839491fa92a09f23281
Hang Su <tonyfloater@gmail.com>
2025-04-17 14:36:21 -0400

    clean up tests

--- poly_commit/src/orion/code_switching_test.rs
+++ poly_commit/src/orion/code_switching_test.rs
@@ -19,67 +19,6 @@ use crate::{
     ORION_CODE_PARAMETER_INSTANCE,
 };
 
-fn test_orion_code_switch_circuit_evaluate_helper<F, C>(num_vars: usize)
-where
-    F: Field,
-    C: FieldEngine<CircuitField = F, ChallengeField = F, SimdCircuitField = F>,
-{
-    const PROXIMITY_REPETITIONS: usize = 2;
-
-    let mut rng = test_rng();
-
-    let msg_size = 1 << num_vars;
-    let encoder = OrionCode::new(ORION_CODE_PARAMETER_INSTANCE, msg_size, &mut rng);
-
-    let evals_poly = MultiLinearPoly::<C::SimdCircuitField>::random(num_vars, &mut rng);
-    let prox_poly0 = MultiLinearPoly::<C::SimdCircuitField>::random(num_vars, &mut rng);
-    let prox_poly1 = MultiLinearPoly::<C::SimdCircuitField>::random(num_vars, &mut rng);
-
-    let input_coeffs = prepare_code_switching_inputs(
-        &evals_poly.coeffs,
-        &[prox_poly0.coeffs.clone(), prox_poly1.coeffs.clone()],
-    );
-
-    let challenge_point: Vec<_> = (0..num_vars)
-        .map(|_| C::ChallengeField::random_unsafe(&mut rng))
-        .collect();
-
-    let mut layered_circuit =
-        code_switching_gkr_circuit::<F, C>(&encoder, &challenge_point, PROXIMITY_REPETITIONS);
-
-    layered_circuit.layers[0].input_vals = input_coeffs.clone();
-
-    layered_circuit.evaluate();
-
-    let expected = {
-        let mut evals_encoded = encoder.encode(&evals_poly.coeffs).unwrap();
-        evals_encoded.resize(msg_size * 2, F::ZERO);
-
-        let mut prox0_encoded = encoder.encode(&prox_poly0.coeffs).unwrap();
-        prox0_encoded.resize(msg_size * 2, F::ZERO);
-
-        let mut prox1_encoded = encoder.encode(&prox_poly1.coeffs).unwrap();
-        prox1_encoded.resize(msg_size * 2, F::ZERO);
-
-        let evaluation = evals_poly.evaluate_jolt(&challenge_point);
-        let mut buffer = vec![evaluation];
-        buffer.resize(msg_size * 2, F::ZERO);
-
-        buffer.extend(evals_encoded);
-        buffer.extend(prox0_encoded);
-        buffer.extend(prox1_encoded);
-
-        buffer
-    };
-
-    assert_eq!(expected, layered_circuit.layers.last().unwrap().output_vals);
-}
-
-#[test]
-fn test_orion_code_switch_circuit_evaluate() {
-    test_orion_code_switch_circuit_evaluate_helper::<Fr, BN254Config>(15);
-}
-
 fn test_orion_code_switch_gkr_helper<F, C>(num_vars: usize, mpi_config: &MPIConfig)
 where
     F: Field + ExtensionField + SimdField,
@@ -112,38 +51,65 @@ where
     layered_circuit.layers[0].input_vals = input_coeffs.clone();
     layered_circuit.evaluate();
 
-    let mut sp = prepare_code_switching_gkr_prover_mem(&layered_circuit);
-    let mut fs_transcript_prover = BytesHashTranscript::<F, Keccak256hasher>::new();
-    let mut fs_transcript_verifier = fs_transcript_prover.clone();
+    {
+        let expected = {
+            let mut evals_encoded = encoder.encode(&evals_poly.coeffs).unwrap();
+            evals_encoded.resize(msg_size * 2, F::ZERO);
 
-    let (claimed_v, challenge_prover) = gkr_prove(
-        &layered_circuit,
-        &mut sp,
-        &mut fs_transcript_prover,
-        mpi_config,
-    );
-    assert!(challenge_prover.rz_1.is_none());
+            let mut prox0_encoded = encoder.encode(&prox_poly0.coeffs).unwrap();
+            prox0_encoded.resize(msg_size * 2, F::ZERO);
 
-    let proof_bytes = fs_transcript_prover.finalize_and_get_proof();
-    let mut proof_reader = Cursor::new(&proof_bytes.bytes);
-    let (verified, challenge_verifier, claimed_v0_verifier, claimed_v1_verifier) = gkr_verify(
-        CODE_SWITCHING_WORLD_SIZE,
-        &layered_circuit,
-        &[],
-        &claimed_v,
-        &mut fs_transcript_verifier,
-        &mut proof_reader,
-    );
-    assert!(challenge_verifier.rz_1.is_none());
+            let mut prox1_encoded = encoder.encode(&prox_poly1.coeffs).unwrap();
+            prox1_encoded.resize(msg_size * 2, F::ZERO);
 
-    assert!(claimed_v1_verifier.is_none());
-    assert_eq!(&challenge_verifier.rz_0, &challenge_prover.rz_0);
-    assert!(verified);
+            let evaluation = evals_poly.evaluate_jolt(&challenge_point);
+            let mut buffer = vec![evaluation];
+            buffer.resize(msg_size * 2, F::ZERO);
 
-    let input_mle = MultiLinearPoly::new(input_coeffs);
-    let expected_final_claim_v = input_mle.evaluate_jolt(&challenge_verifier.rz_0);
+            buffer.extend(evals_encoded);
+            buffer.extend(prox0_encoded);
+            buffer.extend(prox1_encoded);
 
-    assert_eq!(claimed_v0_verifier, expected_final_claim_v);
+            buffer
+        };
+
+        assert_eq!(expected, layered_circuit.layers.last().unwrap().output_vals);
+    }
+
+    {
+        let mut sp = prepare_code_switching_gkr_prover_mem(&layered_circuit);
+        let mut fs_transcript_prover = BytesHashTranscript::<F, Keccak256hasher>::new();
+        let mut fs_transcript_verifier = fs_transcript_prover.clone();
+
+        let (claimed_v, challenge_prover) = gkr_prove(
+            &layered_circuit,
+            &mut sp,
+            &mut fs_transcript_prover,
+            mpi_config,
+        );
+        assert!(challenge_prover.rz_1.is_none());
+
+        let proof_bytes = fs_transcript_prover.finalize_and_get_proof();
+        let mut proof_reader = Cursor::new(&proof_bytes.bytes);
+        let (verified, challenge_verifier, claimed_v0_verifier, claimed_v1_verifier) = gkr_verify(
+            CODE_SWITCHING_WORLD_SIZE,
+            &layered_circuit,
+            &[],
+            &claimed_v,
+            &mut fs_transcript_verifier,
+            &mut proof_reader,
+        );
+        assert!(challenge_verifier.rz_1.is_none());
+
+        assert!(claimed_v1_verifier.is_none());
+        assert_eq!(&challenge_verifier.rz_0, &challenge_prover.rz_0);
+        assert!(verified);
+
+        let input_mle = MultiLinearPoly::new(input_coeffs);
+        let expected_final_claim_v = input_mle.evaluate_jolt(&challenge_verifier.rz_0);
+
+        assert_eq!(claimed_v0_verifier, expected_final_claim_v);
+    }
 }
 
 #[test]
