94bb70927e9e2ce735645910285897888d8af8c5
Zhiyong Fang <zhiyong.fang.1997@gmail.com>
2025-04-30 01:20:10 -0500

    refactor

--- gkr/src/prover.rs
+++ gkr/src/prover.rs
@@ -1,8 +1,8 @@
-pub mod gkr;
-pub use gkr::*;
+pub mod gkr_vanilla;
+pub use gkr_vanilla::*;
 
 pub mod gkr_square;
 pub use gkr_square::*;
 
-pub mod linear_gkr;
-pub use linear_gkr::*;
+pub mod snark;
+pub use snark::*;
