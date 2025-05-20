6cef0f82eea6f991bad5074844c1282010d394e4
zhenfei <zhenfei.zhang@hotmail.com>
2025-05-20 09:27:56 -0400

    bring back the tests

--- poly_commit/tests/test_hyrax.rs
+++ poly_commit/tests/test_hyrax.rs
@@ -1,83 +1,83 @@
-// mod common;
+mod common;
 
-// use arith::{Field, Fr};
-// use ark_std::test_rng;
-// use gkr_engine::{BN254Config, ExpanderSingleVarChallenge, MPIConfig, MPIEngine, Transcript};
-// use gkr_hashers::Keccak256hasher;
-// use halo2curves::bn256::G1Affine;
-// use poly_commit::HyraxPCS;
-// use polynomials::MultiLinearPoly;
-// use transcript::BytesHashTranscript;
+use arith::{Field, Fr};
+use ark_std::test_rng;
+use gkr_engine::{BN254Config, ExpanderSingleVarChallenge, MPIConfig, MPIEngine, Transcript};
+use gkr_hashers::Keccak256hasher;
+use halo2curves::bn256::G1Affine;
+use poly_commit::HyraxPCS;
+use polynomials::MultiLinearPoly;
+use transcript::BytesHashTranscript;
 
-// const TEST_REPETITION: usize = 3;
+const TEST_REPETITION: usize = 3;
 
-// fn test_hyrax_pcs_generics(num_vars_start: usize, num_vars_end: usize) {
-//     let mut rng = test_rng();
+fn test_hyrax_pcs_generics(num_vars_start: usize, num_vars_end: usize) {
+    let mut rng = test_rng();
 
-//     (num_vars_start..=num_vars_end).for_each(|num_vars| {
-//         let xs: Vec<_> = (0..TEST_REPETITION)
-//             .map(|_| -> Vec<Fr> { (0..num_vars).map(|_| Fr::random_unsafe(&mut rng)).collect() })
-//             .collect();
-//         let poly = MultiLinearPoly::<Fr>::random(num_vars, &mut rng);
+    (num_vars_start..=num_vars_end).for_each(|num_vars| {
+        let xs: Vec<_> = (0..TEST_REPETITION)
+            .map(|_| -> Vec<Fr> { (0..num_vars).map(|_| Fr::random_unsafe(&mut rng)).collect() })
+            .collect();
+        let poly = MultiLinearPoly::<Fr>::random(num_vars, &mut rng);
 
-//         common::test_pcs::<Fr, BytesHashTranscript<Keccak256hasher>, HyraxPCS<G1Affine>>(
-//             &num_vars, &poly, &xs,
-//         );
-//     })
-// }
+        common::test_pcs::<Fr, BytesHashTranscript<Keccak256hasher>, HyraxPCS<G1Affine>>(
+            &num_vars, &poly, &xs,
+        );
+    })
+}
 
-// #[test]
-// fn test_hyrax_pcs_e2e() {
-//     test_hyrax_pcs_generics(3, 17)
-// }
+#[test]
+fn test_hyrax_pcs_e2e() {
+    test_hyrax_pcs_generics(3, 17)
+}
 
-// fn test_hyrax_for_expander_gkr_generics(mpi_config_ref: &MPIConfig, total_num_vars: usize) {
-//     let mut rng = test_rng();
+fn test_hyrax_for_expander_gkr_generics(mpi_config_ref: &MPIConfig, total_num_vars: usize) {
+    let mut rng = test_rng();
 
-//     // NOTE BN254 GKR SIMD pack size = 1, num vars in SIMD is 0
-//     let num_vars_in_mpi = mpi_config_ref.world_size().ilog2() as usize;
-//     let num_vars_in_each_poly = total_num_vars - num_vars_in_mpi;
+    // NOTE BN254 GKR SIMD pack size = 1, num vars in SIMD is 0
+    let num_vars_in_mpi = mpi_config_ref.world_size().ilog2() as usize;
+    let num_vars_in_each_poly = total_num_vars - num_vars_in_mpi;
 
-//     let global_poly = MultiLinearPoly::<Fr>::random(total_num_vars, &mut rng);
-//     let challenge_point = ExpanderSingleVarChallenge::<BN254Config> {
-//         r_mpi: (0..num_vars_in_mpi)
-//             .map(|_| Fr::random_unsafe(&mut rng))
-//             .collect(),
-//         r_simd: Vec::new(),
-//         rz: (0..num_vars_in_each_poly)
-//             .map(|_| Fr::random_unsafe(&mut rng))
-//             .collect(),
-//     };
+    let global_poly = MultiLinearPoly::<Fr>::random(total_num_vars, &mut rng);
+    let challenge_point = ExpanderSingleVarChallenge::<BN254Config> {
+        r_mpi: (0..num_vars_in_mpi)
+            .map(|_| Fr::random_unsafe(&mut rng))
+            .collect(),
+        r_simd: Vec::new(),
+        rz: (0..num_vars_in_each_poly)
+            .map(|_| Fr::random_unsafe(&mut rng))
+            .collect(),
+    };
 
-//     let mut transcript = BytesHashTranscript::<Keccak256hasher>::new();
+    let mut transcript = BytesHashTranscript::<Keccak256hasher>::new();
 
-//     // NOTE separate polynomial into different pieces by mpi rank
-//     let poly_vars_stride = (1 << global_poly.get_num_vars()) / mpi_config_ref.world_size();
-//     let poly_coeff_starts = mpi_config_ref.world_rank() * poly_vars_stride;
-//     let poly_coeff_ends = poly_coeff_starts + poly_vars_stride;
-//     let local_poly =
-//         MultiLinearPoly::new(global_poly.coeffs[poly_coeff_starts..poly_coeff_ends].to_vec());
+    // NOTE separate polynomial into different pieces by mpi rank
+    let poly_vars_stride = (1 << global_poly.get_num_vars()) / mpi_config_ref.world_size();
+    let poly_coeff_starts = mpi_config_ref.world_rank() * poly_vars_stride;
+    let poly_coeff_ends = poly_coeff_starts + poly_vars_stride;
+    let local_poly =
+        MultiLinearPoly::new(global_poly.coeffs[poly_coeff_starts..poly_coeff_ends].to_vec());
 
-//     dbg!(local_poly.get_num_vars(), local_poly.coeffs[0]);
+    dbg!(local_poly.get_num_vars(), local_poly.coeffs[0]);
 
-//     common::test_pcs_for_expander_gkr::<
-//         BN254Config,
-//         BytesHashTranscript<Keccak256hasher>,
-//         HyraxPCS<G1Affine>,
-//     >(
-//         &num_vars_in_each_poly,
-//         mpi_config_ref,
-//         &mut transcript,
-//         &local_poly,
-//         &[challenge_point],
-//     );
-// }
+    common::test_pcs_for_expander_gkr::<
+        BN254Config,
+        BytesHashTranscript<Keccak256hasher>,
+        HyraxPCS<G1Affine>,
+    >(
+        &num_vars_in_each_poly,
+        mpi_config_ref,
+        &mut transcript,
+        &local_poly,
+        &[challenge_point],
+    );
+}
 
-// #[test]
-// fn test_hyrax_for_expander_gkr() {
-//     let mpi_config = MPIConfig::prover_new();
+#[test]
+fn test_hyrax_for_expander_gkr() {
+    let mpi_config = MPIConfig::prover_new();
 
-//     test_hyrax_for_expander_gkr_generics(&mpi_config, 19);
+    test_hyrax_for_expander_gkr_generics(&mpi_config, 19);
 
-//     MPIConfig::finalize()
-// }
+    MPIConfig::finalize()
+}
--- poly_commit/tests/test_raw.rs
+++ poly_commit/tests/test_raw.rs
@@ -1,73 +1,73 @@
-// mod common;
+mod common;
 
-// use arith::{Field, Fr};
-// use gkr_engine::{
-//     BN254Config, ExpanderSingleVarChallenge, FieldEngine, GF2ExtConfig, M31x16Config, MPIConfig,
-//     MPIEngine, Transcript,
-// };
-// use gkr_hashers::{Keccak256hasher, SHA256hasher};
-// use poly_commit::raw::{RawExpanderGKR, RawMultiLinearPCS};
-// use polynomials::{MultiLinearPoly, RefMultiLinearPoly};
-// use rand::thread_rng;
-// use transcript::BytesHashTranscript;
+use arith::{Field, Fr};
+use gkr_engine::{
+    BN254Config, ExpanderSingleVarChallenge, FieldEngine, GF2ExtConfig, M31x16Config, MPIConfig,
+    MPIEngine, Transcript,
+};
+use gkr_hashers::{Keccak256hasher, SHA256hasher};
+use poly_commit::raw::{RawExpanderGKR, RawMultiLinearPCS};
+use polynomials::{MultiLinearPoly, RefMultiLinearPoly};
+use rand::thread_rng;
+use transcript::BytesHashTranscript;
 
-// #[test]
-// fn test_raw() {
-//     // NOTE(HS) 8 variables
-//     let params = 8;
-//     let mut rng = thread_rng();
-//     let poly = MultiLinearPoly::random(params, &mut rng);
-//     let xs = (0..100)
-//         .map(|_| {
-//             (0..params)
-//                 .map(|_| Fr::random_unsafe(&mut rng))
-//                 .collect::<Vec<Fr>>()
-//         })
-//         .collect::<Vec<Vec<Fr>>>();
+#[test]
+fn test_raw() {
+    // NOTE(HS) 8 variables
+    let params = 8;
+    let mut rng = thread_rng();
+    let poly = MultiLinearPoly::random(params, &mut rng);
+    let xs = (0..100)
+        .map(|_| {
+            (0..params)
+                .map(|_| Fr::random_unsafe(&mut rng))
+                .collect::<Vec<Fr>>()
+        })
+        .collect::<Vec<Vec<Fr>>>();
 
-//     common::test_pcs::<Fr, BytesHashTranscript<Keccak256hasher>, RawMultiLinearPCS>(
-//         &params, &poly, &xs,
-//     );
-// }
+    common::test_pcs::<Fr, BytesHashTranscript<Keccak256hasher>, RawMultiLinearPCS>(
+        &params, &poly, &xs,
+    );
+}
 
-// fn test_raw_gkr_helper<C: FieldEngine, T: Transcript>(mpi_config: &MPIConfig, transcript: &mut T)
-// {     // NOTE(HS) local variables being 8
-//     let params = 8;
-//     let mut rng = thread_rng();
-//     let hypercube_basis = (0..(1 << params))
-//         .map(|_| C::SimdCircuitField::random_unsafe(&mut rng))
-//         .collect();
-//     let poly = RefMultiLinearPoly::from_ref(&hypercube_basis);
-//     let xs = (0..100)
-//         .map(|_| ExpanderSingleVarChallenge::<C> {
-//             rz: (0..params)
-//                 .map(|_| C::ChallengeField::random_unsafe(&mut rng))
-//                 .collect::<Vec<C::ChallengeField>>(),
-//             r_simd: (0..C::get_field_pack_size().trailing_zeros())
-//                 .map(|_| C::ChallengeField::random_unsafe(&mut rng))
-//                 .collect::<Vec<C::ChallengeField>>(),
-//             r_mpi: (0..mpi_config.world_size().trailing_zeros())
-//                 .map(|_| C::ChallengeField::random_unsafe(&mut rng))
-//                 .collect::<Vec<C::ChallengeField>>(),
-//         })
-//         .collect::<Vec<ExpanderSingleVarChallenge<C>>>();
-//     common::test_pcs_for_expander_gkr::<C, T, RawExpanderGKR<C>>(
-//         &params, mpi_config, transcript, &poly, &xs,
-//     );
-// }
+fn test_raw_gkr_helper<C: FieldEngine, T: Transcript>(mpi_config: &MPIConfig, transcript: &mut T)
+{     // NOTE(HS) local variables being 8
+    let params = 8;
+    let mut rng = thread_rng();
+    let hypercube_basis = (0..(1 << params))
+        .map(|_| C::SimdCircuitField::random_unsafe(&mut rng))
+        .collect();
+    let poly = RefMultiLinearPoly::from_ref(&hypercube_basis);
+    let xs = (0..100)
+        .map(|_| ExpanderSingleVarChallenge::<C> {
+            rz: (0..params)
+                .map(|_| C::ChallengeField::random_unsafe(&mut rng))
+                .collect::<Vec<C::ChallengeField>>(),
+            r_simd: (0..C::get_field_pack_size().trailing_zeros())
+                .map(|_| C::ChallengeField::random_unsafe(&mut rng))
+                .collect::<Vec<C::ChallengeField>>(),
+            r_mpi: (0..mpi_config.world_size().trailing_zeros())
+                .map(|_| C::ChallengeField::random_unsafe(&mut rng))
+                .collect::<Vec<C::ChallengeField>>(),
+        })
+        .collect::<Vec<ExpanderSingleVarChallenge<C>>>();
+    common::test_pcs_for_expander_gkr::<C, T, RawExpanderGKR<C>>(
+        &params, mpi_config, transcript, &poly, &xs,
+    );
+}
 
-// #[test]
-// fn test_raw_gkr() {
-//     let mpi_config = MPIConfig::prover_new();
+#[test]
+fn test_raw_gkr() {
+    let mpi_config = MPIConfig::prover_new();
 
-//     type TM31 = BytesHashTranscript<Keccak256hasher>;
-//     test_raw_gkr_helper::<M31x16Config, TM31>(&mpi_config, &mut TM31::new());
+    type TM31 = BytesHashTranscript<Keccak256hasher>;
+    test_raw_gkr_helper::<M31x16Config, TM31>(&mpi_config, &mut TM31::new());
 
-//     type TGF2 = BytesHashTranscript<SHA256hasher>;
-//     test_raw_gkr_helper::<GF2ExtConfig, TGF2>(&mpi_config, &mut TGF2::new());
+    type TGF2 = BytesHashTranscript<SHA256hasher>;
+    test_raw_gkr_helper::<GF2ExtConfig, TGF2>(&mpi_config, &mut TGF2::new());
 
-//     type TBN254 = BytesHashTranscript<Keccak256hasher>;
-//     test_raw_gkr_helper::<BN254Config, TBN254>(&mpi_config, &mut TBN254::new());
+    type TBN254 = BytesHashTranscript<Keccak256hasher>;
+    test_raw_gkr_helper::<BN254Config, TBN254>(&mpi_config, &mut TBN254::new());
 
-//     MPIConfig::finalize();
-// }
+    MPIConfig::finalize();
+}
