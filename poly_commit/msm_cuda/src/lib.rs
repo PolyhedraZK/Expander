bd840644c26d4f455855bb6299145a9913210a9b
Zhiyong Fang <zhiyong.fang.1997@gmail.com>
2025-08-29 12:47:08 -0700

    comment

--- poly_commit/msm_cuda/src/lib.rs
+++ poly_commit/msm_cuda/src/lib.rs
@@ -2,6 +2,9 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+// The feature cuda does not have to be manually enabled
+// build.rs has already taken care of that
+
 pub mod util;
 
 #[cfg(feature = "cuda")]
